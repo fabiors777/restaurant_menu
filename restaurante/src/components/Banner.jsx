eca569b24ef4c64d0168abb484f935512baddefc
Fábio Ribeiro de Souza <55698885+fabiors777@users.noreply.github.com>
2025-10-16 19:14:36 -0300

    First load of files

---
+++ restaurante/src/components/Banner.jsx
@@ -0,0 +1,11 @@
+import imgRestaurante from "../assets/restaurante.webp";
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <img src={imgRestaurante} alt="Banner do Restaurante" />
+    </div>
+  );
+};
+
+export default Banner;
